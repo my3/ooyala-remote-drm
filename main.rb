48c8ebcd4ddee40211423cf98a756b73892e16e8
Geoffrey Giacoman <geoffrey@ooyala.com>
2017-07-06 13:34:05 -0500

    Added ability to generate Fairplay keys for a remote asset

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'ooyala-v2-api'
 
+require_relative 'fairplay'
 require_relative 'widevine_modular'
 
 config_file = File.read('config.json')
@@ -8,34 +9,22 @@ config = JSON.parse(config_file)
 
 api = Ooyala::API.new(config['api_key'], config['api_secret'])
 
-asset = {
-            :name => "My Live Remote Asset",
-            :asset_type => "remote_asset",
-            :is_live_stream => true
-        }
+fairplay_keys = Fairplay.new.request_key(api, 'VveWJ0YjE6A_SetNdTe9E7TQyvXHQ5Rd')
+puts "Keys to use for HLS Fairplay: #{fairplay_keys}"
 
-response = api.post('assets', asset)
-embed_code = response['embed_code']
-
-puts "Created a movie in Backlot with embed_code #{embed_code}"
-
-# Request keys for HLS Fairplay
-response = api.post("assets/#{embed_code}/drm_attributes/fps")
-puts "Keys to use for HLS Fairplay: #{response}"
-
-# Request keys for DASH CENC
+Request keys for DASH CENC
 wv_keys = WidevineModular.new.request_key(config['key_server'], 
-                            config['wv_aes_key'], 
-                            config['wv_aes_iv'], 
-                            config['wv_provider_id'],
-                            embed_code)
+                           config['wv_aes_key'], 
+                           config['wv_aes_iv'], 
+                           config['wv_provider_id'],
+                           embed_code)
 
 puts "Keys to use for DASH CENC: #{wv_keys}"
 
 
-# Encode assets at this point with keys above
+Encode assets at this point with keys above
 
-# After encoding, need to set the movie_urls on the asset
+After encoding, need to set the movie_urls on the asset
 movie_urls = { 
                 :dash => "https://dash.ooyala.com/#{embed_code}.mpd",
                 :hls => "https://hls.ooyala.com/#{embed_code}.m3u8"
