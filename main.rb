ccb30612285cdef7e1205b7f52ade19b627c0646
Geoffrey Giacoman <geoffrey@ooyala.com>
2017-07-11 13:45:10 -0500

    Add command line arguments to determine to generate keys only or to generate keys and create remote asset

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'ooyala-v2-api'
+require 'optparse'
 
 require_relative 'fairplay'
 require_relative 'widevine_modular'
@@ -9,14 +10,39 @@ config = JSON.parse(config_file)
 
 api = Ooyala::API.new(config['api_key'], config['api_secret'])
 
-asset = {
-            :name => "My Live Remote Asset",
+options = {}
+OptionParser.new do |opt|
+    opt.on('--embed-code EMBEDCODE', 'Embed code to generate keys for') { |o| options[:embed_code] = o }
+    opt.on('--name NAME', 'Name of remote asset to create') { |o| options[:name] = o }
+    opt.on('--h', 'Shows possible command line arguments') do 
+        puts opt
+        exit
+    end
+end.parse!
+
+if options[:embed_code] and options[:name]
+    puts "You must only supply one of embed-code or name"
+    exit
+end
+
+if not options[:embed_code] and not options[:name]
+    puts "You must supply either an embed code or name to generate keys"
+    exit
+end
+
+embed_code = ""
+if options[:embed_code]
+    embed_code = options[:embed_code]
+else
+    asset = {
+            :name => options[:name],
             :asset_type => "remote_asset",
             :is_live_stream => true
         }
 
-response = api.post('assets', asset)
-embed_code = response['embed_code']
+    response = api.post('assets', asset)
+    embed_code = response['embed_code']
+end
 
 puts "Created a movie in Backlot with embed_code #{embed_code}"
 
